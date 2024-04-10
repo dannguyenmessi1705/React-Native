a35c9f669426026ce66b23fe1b867078c60547a3
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-04-11 01:48:12 +0700

    ui: improve layout

--- 01.RNProject/App.js
+++ 01.RNProject/App.js
@@ -4,30 +4,42 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your name" />
+        <TextInput style={styles.textInput} placeholder="Your course goal" />
         {/* Tạo input, trong RN sẽ là self close tag */}
-        <Button title="Add text" />
-        {/* Tạo button, trong RN sẽ là self close tag */}
+        <Button title="Add course" />
+        {/* Tạo button, trong RN sẽ là self close tag, Button trong RN sẽ không có thẻ style */}
       </View>
-      <Text>List of app...</Text>
+      <View style={styles.contentContainer}>
+        <Text>List of courses...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
+    alignItems: "center",
     justifyContent: "space-between",
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+    marginBottom: 24,
   },
   textInput: {
     borderWidth: 1,
     borderColor: "#cccccc",
-    width: "80%",
+    width: "70%",
     margin: 8,
     padding: 8,
+  },
+  contentContainer: {
+    flex: 4,
   },
 });
 // Tạo style cho từng attribute
