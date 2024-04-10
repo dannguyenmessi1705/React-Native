52f211b4dbc9bb16ea67e31f111306b3ecaa159a
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-04-10 23:08:29 +0700

    docs: how to style for element

--- 01.RNProject/App.js
+++ 01.RNProject/App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Text style={styles.dummyText}>Hello World</Text>
+      </View>
+      <Text style={styles.dummyText}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title="Start" />
     </View>
   );
 }
@@ -13,8 +17,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-});
+  dummyText: {
+    borderWidth: 2,
+    borderColor: "red",
+    margin: 16,
+    padding: 16,
+  }
+}); // Tạo style cho từng attribute
