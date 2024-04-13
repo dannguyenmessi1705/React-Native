9c49f16b3e18d5337c8f58802dddff338b736a93
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-04-14 05:10:32 +0700

    feat: use expo-linear-gradient

--- 02.MiniGame/App.js
+++ 02.MiniGame/App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 import StartGameScreen from "./screen/StartGameScreen";
 
 export default function App() {
-  return <StartGameScreen />;
+  return (
+    <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.container}>
+      <StartGameScreen />
+    </LinearGradient>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
