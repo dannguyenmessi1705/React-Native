9b6fc080232576f67bf53decd10e29e4f9a01ae8
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-07-30 01:30:32 +0700

    Init Project Navigation

---
+++ 03.NavigationMealsApp/screen/CategoryScreen.js
@@ -0,0 +1,22 @@
+import { FlatList } from "react-native";
+
+import { CATEGORIES } from "../data/dummy-data";
+import CategoryGridTile from "../components/CategoryGridTile";
+
+const renderCategoryItem = (itemData) => {
+  return (
+    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+  );
+};
+
+function CategoryScreen() {
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={(item) => renderCategoryItem(item)}
+    />
+  );
+}
+
+export default CategoryScreen;
