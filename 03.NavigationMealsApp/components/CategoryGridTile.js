d9ecf6cefa516600418c819c8ed08d63544a0ca2
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-07-31 21:22:12 +0700

    Grid Item

--- 03.NavigationMealsApp/components/CategoryGridTile.js
+++ 03.NavigationMealsApp/components/CategoryGridTile.js
@@ -1,11 +1,17 @@
-import { Pressable, View, Text } from "react-native";
+import { Pressable, View, Text, StyleSheet, Platform } from "react-native";
 
 function CategoryGridTile({ title, color }) {
   return (
-    <View>
-      <Pressable>
-        <View>
-          <Text>{title}</Text>
+    <View style={styles.gridItem}>
+      <Pressable
+        android_ripple={{ color: "#fff" }}
+        style={({ pressed }) => [
+          styles.button,
+          pressed ? styles.buttonPress : null,
+        ]}
+      >
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
+          <Text style={styles.title}>{title}</Text>
         </View>
       </Pressable>
     </View>
@@ -13,3 +19,36 @@ function CategoryGridTile({ title, color }) {
 }
 
 export default CategoryGridTile;
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    elevation: 4,
+    backgroundColor: "white",
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowOffset: { height: 2, width: 0 },
+    shadowRadius: 8,
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+  },
+  button: {
+    flex: 1,
+  },
+  buttonPress: {
+    opacity: 0.5,
+  },
+  innerContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+    borderRadius: 8,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
